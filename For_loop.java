a28f5472bad257e2ced5359acc1d991045fbd600
roshnigrace <roshnigrace12@gmail.com>
2024-11-28 12:09:48 +0530

    new

---
+++ For_loop.java
@@ -0,0 +1,12 @@
+package basic_java_programs;
+
+public class For_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=10;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
